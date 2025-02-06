0922a704fbb8168613cd17adf7313fdb03ded484
HanyiYe0 <hanyiye2@gmail.com>
2025-02-06 16:00:19 -0500

    Initial Commit

---
+++ habit-tracker-hive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  habit-tracker-hive
+//
+//  Created by Hanyi Ye on 2025-02-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ habit-tracker-hive/habit_tracker_hiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_tracker_hiveApp.swift
+//  habit-tracker-hive
+//
+//  Created by Hanyi Ye on 2025-02-06.
+//
+
+import SwiftUI
+
+@main
+struct habit_tracker_hiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
